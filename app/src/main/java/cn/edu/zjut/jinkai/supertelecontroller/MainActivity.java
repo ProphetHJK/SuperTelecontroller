2b9010fce55130da9dfe4bbe49ca93b9b0e04185
Jinkai <1256743772@qq.com>
2018-06-11 12:01:09 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/zjut/jinkai/supertelecontroller/MainActivity.java
@@ -0,0 +1,47 @@
+package cn.edu.zjut.jinkai.supertelecontroller;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private TextView sshView;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    //mTextMessage.setText(R.string.title_dashboard);
+                    sshView.setVisibility(TextView.VISIBLE);
+                    mTextMessage.setVisibility(TextView.GONE);
+                    return true;
+                case R.id.navigation_ssh:
+                    mTextMessage.setText(R.string.title_ssh);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sshView=(TextView)findViewById(R.id.ssh);
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
